9e011c73aca971d74b29e026ab9707464065e8cb
Thor McAvenia <mcaveniathor@gmail.com>
2018-05-17 16:49:34 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+#/usr/bin/python3
+import price
+price.write("BTCUSDT", .0009)
---
+++ price.py
@@ -0,0 +1,40 @@
+import requests
+import csv
+import time
+import threading
+filename = "binance_price.csv"
+
+class priceThread(threading.Thread):
+    def __init__(self, symbol, threadID):
+        threading.Thread.__init__(self)
+        self.symbol = symbol
+        self.threadID = threadID
+
+def getPrice(sym):
+        payload={"symbol":sym}
+        priceDict= requests.get("https://api.binance.com/api/v3/ticker/price", payload).json()
+        priceDict["time"] = time.time()
+        return priceDict
+def write(sym, delay):
+    with open("price.csv", "a") as csvfile:
+        fieldnames = ["symbol", "price", "time"]
+        priceWriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        while True:
+           priceDict = getPrice(sym)
+           priceWriter.writerow(priceDict)
+           time.sleep(delay)
+symbols = ["BTCUSDT", "ETHUSDT"]
+class ThreadPool():
+    threads = []
+    def __init__(self, symbols):
+        counter = 0
+        for symbol in symbols:
+            threads.append(PriceThread(i, counter))
+            threads[counter].start()
+            counter += 1
+    def joinThreads()
+        for thread in threads:
+            thread.join()
+
+
+
